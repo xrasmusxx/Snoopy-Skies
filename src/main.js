03e39301b017de497e5083d5b9b5018bf05b41c1
Rasmus <rasmuspolluaar0@gmail.com>
2026-01-13 09:36:49 +0200

    bg

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ kaboom({ pixelDensity: 1 });
 // LOAD SPRITES
 loadSprite("snoop1", "/sprites/gd.png");
 loadSprite("snoop2", "/sprites/gd.png");
-loadSprite("bg", "/sprites/gd-backg.png");
+loadSprite("bg", "/sprites/gd-backg-green.png");
 loadSprite("doghouse", "/sprites/doghouse.png");
 loadSprite("house", "/sprites/house.png");
 loadSprite("ballon", "/sprites/ballon.png");
